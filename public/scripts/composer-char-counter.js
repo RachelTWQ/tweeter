784cce65585bde387d2e8c1e1a3cac9ac385989e
RachelTWQ <racheltang15@hotmail.com>
2019-04-16 18:33:58 +0000

    apply jQuery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
-    let input = document.querySelector(".new-tweet textarea");
-    let spanText = document.querySelector(".counter");
-    input.oninput = function handleInput(e) {
-        let remain = 140 - e.target.value.length;
-        spanText.textContent = remain;
-        if (remain >= 0){
-            spanText.style.color = "#244751";
+    // let input = document.querySelector(".new-tweet textarea");
+    // let spanText = document.querySelector(".counter");
+    // input.oninput = function handleInput(e) {
+    //     let remain = 140 - e.target.value.length;
+    //     spanText.textContent = remain;
+    //     if (remain >= 0){
+    //         spanText.style.color = "#244751";
+    //     } else {
+    //         spanText.style.color = "red";
+    //     }
+    // }
+
+    $(".new-tweet textarea").on("input", function(){
+        let remain = 140 - $(this).val().length;
+        let target = $(this).siblings().children()[1];
+        target.innerText = remain;
+        if (remain >= 0) {
+            target.style.color = "#244751";
         } else {
-            spanText.style.color = "red";
+            target.style.color = "#FF0000";
         }
-    }
+    })
   });
