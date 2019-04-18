4e2a39f5c70a69daff352a2d7650bd62381f6be9
RachelTWQ <racheltang15@hotmail.com>
2019-04-18 21:04:14 +0000

    remove comment

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,69 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const tweetData = {
-//     "user": {
-//         "name": "Newton",
-//         "avatars": {
-//             "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//             "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//             "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//         },
-//         "handle": "@SirIsaac"
-//     },
-//     "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-// }
-
-// const data = [
-//     {
-//         "user": {
-//             "name": "Newton",
-//             "avatars": {
-//                 "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//                 "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//                 "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//             },
-//             "handle": "@SirIsaac"
-//         },
-//         "content": {
-//             "text": "If I have seen further it is by standing on the shoulders of giants"
-//         },
-//         "created_at": 1461116232227
-//     },
-//     {
-//         "user": {
-//             "name": "Descartes",
-//             "avatars": {
-//                 "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//                 "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//                 "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//             },
-//             "handle": "@rd"
-//         },
-//         "content": {
-//             "text": "Je pense , donc je suis"
-//         },
-//         "created_at": 1461113959088
-//     },
-//     {
-//         "user": {
-//             "name": "Johann von Goethe",
-//             "avatars": {
-//                 "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//                 "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//                 "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//             },
-//             "handle": "@johann49"
-//         },
-//         "content": {
-//             "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//         },
-//         "created_at": 1461113796368
-//     }
-// ];
 const date = moment();
 
 function createTweetElement(data) {
@@ -110,9 +47,7 @@ function loadTweets() {
 
 function charCounter() {
     let $text = $(".new-tweet textarea");
-    // console.log('$text ', $text.val());
     let remain = 140 - $text.val().length;
-    // let target = $text.siblings().children()[1];
     let target = $(".counter").get(0);
     target.innerText = remain;
     if (remain >= 0) {
@@ -120,22 +55,10 @@ function charCounter() {
     } else {
         target.style.color = "#FF0000";
     }
-    // let input = document.querySelector(".new-tweet textarea");
-    // let spanText = document.querySelector(".counter");
-    // input.oninput = function handleInput(e) {
-    //     let remain = 140 - e.target.value.length;
-    //     spanText.textContent = remain;
-    //     if (remain >= 0) {
-    //         spanText.style.color = "#244751";
-    //     } else {
-    //         spanText.style.color = "red";
-    //     }
-    // }
 }
 
 $(document).ready(function () {
     $(".new-tweet textarea").on("input", charCounter);
-    // charCounter();
 
     $("form").submit(function (event) {
         event.preventDefault();
@@ -153,13 +76,6 @@ $(document).ready(function () {
         } else if (inputLength > 140) {
             $(".isa_error").append("<i class='fas fa-exclamation-circle'></i><p class ='error_msg'>Your tweet looks a bit long. Let's make it short.</p>").slideDown(100);
 
-            // $(".new-tweet").append("<div class='isa_error'><i class='fas fa-exclamation-circle'></i><p>Your tweet looks a bit long. Let's make it short.</p></div>");
-            // $(".isa_error").slideDown();
-            // let $focus = $("#tweetInput").get(0);
-            // $focus.addEventListener("focus", function(){
-            //     $(".isa_error").slideUp();
-            // })
-
         } else {
             $.ajax({
                 type: "POST",
@@ -174,15 +90,16 @@ $(document).ready(function () {
             });
         }
     });
-    loadTweets()
-    let isCollapsed = false
+    loadTweets();
+    
+    let isCollapsed = true;
     let $button = $("#nav-bar > ul").get(0);
     $button.addEventListener("click", function(){
         $(".new-tweet").slideToggle();
         if(isCollapsed) {
-            $("#tweetInput").focus()
+            $("#tweetInput").focus();
         }
-        isCollapsed = !isCollapsed
+        isCollapsed = !isCollapsed;
     })
 
 });
