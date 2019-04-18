815748d9b46814a706b3cfca391756c7510a147d
RachelTWQ <racheltang15@hotmail.com>
2019-04-18 20:00:29 +0000

    apply mongoDB to getTweets and saveTweets method

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -10,7 +10,8 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        // db.tweets.push(newTweet);
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
       });
     },
@@ -18,8 +19,17 @@ module.exports = function makeDataHelpers(db) {
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        
+        db.collection("tweets").find().toArray(
+          (err, tweets) => {
+            if(err) {
+                throw err;
+            }
+            
+            
+            const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+            callback(null, tweets.sort(sortNewestFirst));
+        });
       });
     }
 
