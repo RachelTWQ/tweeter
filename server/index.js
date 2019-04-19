f71d9e659358dc35238fc96a795d0fb55730f3ad
RachelTWQ <racheltang15@hotmail.com>
2019-04-19 01:27:01 +0000

    organize code

--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
 "use strict";
 
 // Basic express setup:
-
 const PORT = 8080;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-//MongoDB setup
+// MongoDB setup
 const { MongoClient } = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 let db;
+
 MongoClient.connect(MONGODB_URI, (err, mongoDb) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
